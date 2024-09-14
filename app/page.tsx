1099d235e46c1f540582771add53b30e95490e6e
tiesen243 <ttien56906@gmail.com>
2024-09-14 21:24:26 +0700

    feat(ui): enhance code block styling
    - Added shadow to the outer container for better visual depth.
    - Introduced a flex container with buttons for close and minimize.
    - Adjusted padding and margins for better alignment and spacing.

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,18 @@ const Page: NextPage = () => (
         Next.js + TypeScript
       </h1>
 
-      <div className="bg-gradient-to-br grid place-items-center rounded-lg from-success to-warning w-full aspect-video">
-        <div className="bg-secondary/40 h-4/5 p-6 overflow-auto grid place-items-center rounded-lg shadow-lg w-4/5 aspect-video backdrop-blur-xl">
-          <pre className="text-primary">
+      <div className="bg-gradient-to-br grid place-items-center rounded-lg from-success to-warning w-full aspect-video shadow-lg">
+        <div className="bg-secondary/40 h-4/5 rounded-lg shadow-lg w-4/5 aspect-video backdrop-blur-xl">
+          <div className="flex bg-secondary w-full rounded-t-lg p-2 gap-1">
+            <div className="bg-destructive size-4 text-xs text-center text-secondary rounded-full">
+              x
+            </div>
+            <div className="bg-warning size-4 text-xs text-center text-secondary rounded-full">
+              -
+            </div>
+          </div>
+
+          <pre className="text-primary p-6">
             <code>{code}</code>
           </pre>
         </div>

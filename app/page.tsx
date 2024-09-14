7910b17730de097c131a0c8ed42ca5b834976758
tiesen243 <ttien56906@gmail.com>
2024-09-14 21:01:56 +0700

    feat: add success and warning color variables
    - Added success and warning color variables to globals.css
    - Updated page.tsx to use new color variables for gradient text
    - Added @radix-ui/react-slot dependency in package.json
    - Updated tailwind.config.ts to include success and warning colors

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next'
 
 const Page: NextPage = () => (
-  <div className="font-sans">
-    <pre className="font-sans">
-      {`import type { NextPage } from 'next'`}
-      <br />
-      {`const Page: NextPage = () => {  return <div>Page</div> }`}
-    </pre>
-  </div>
+  <main className="grid min-h-dvh place-items-center">
+    <div className="space-y-4">
+      <p className="text-4xl font-bold text-center">Next level of Type Safety with</p>
+      <h1 className="text-6xl text-transparent bg-gradient-to-br from-success to-warning bg-clip-text font-bold text-center">
+        Next.js + TypeScript
+      </h1>
+    </div>
+  </main>
 )
 
 export default Page

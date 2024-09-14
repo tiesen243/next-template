72681fa1a12b26d226fe009db061e8d22c5fbef4
tiesen243 <ttien56906@gmail.com>
2024-09-14 20:27:12 +0700

    feat: update styles and dependencies
    - Removed unused font files and updated global styles.
    - Added new color variables and applied them to the dark theme.
    - Updated layout and page components to use new font imports.
    - Updated package.json with new dependencies and scripts.
    - Simplified postcss and tailwind configuration files.
    - Enhanced tsconfig.json with stricter options and path aliases.

---
+++ prettier.config.js
@@ -0,0 +1,32 @@
+/** @typedef {import('prettier').Config} PrettierConfig */
+/** @typedef {import('prettier-plugin-tailwindcss')} TailwindConfig */
+/** @typedef {import('@ianvs/prettier-plugin-sort-imports')} SortImportsConfig */
+
+/** @type {PrettierConfig | TailwindConfig | SortImportsConfig} */
+const config = {
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+  tailwindFunctions: ['cn', 'cva'],
+  plugins: ['prettier-plugin-tailwindcss', '@ianvs/prettier-plugin-sort-imports'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy', 'moduleStringNames'],
+  importOrderTypeScriptVersion: '5.6.2',
+  importOrder: [
+    '<TYPES>',
+    '^(next/(.+))$',
+    '^(react/(.+))$|^(react)$',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@/components/(.*)$',
+    '_components/(.*)$',
+    '',
+    '<TYPES>^(@/(.+))$',
+    '^(@/server|@/emails|@/hooks|@/lib|@)(/.*)$',
+    '^[..]',
+    '^[.]',
+  ],
+}
+
+export default config

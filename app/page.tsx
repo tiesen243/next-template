7b4619c3801a4e50bb5b6427e21f4b4657d1aa82
tiesen243 <ttien56906@gmail.com>
2024-09-19 17:36:48 +0700

    fix(image): update image URL to new assets path
    Updated the image URL in the page component to point to the new assets
    path. This change ensures that the image is correctly loaded from the
    updated location.

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ const Page: NextPage = () => (
   <main className="container flex min-h-dvh max-w-screen-lg flex-col items-center justify-center overflow-x-hidden">
     <div className="pointer-events-none relative flex place-items-center before:absolute before:h-[700px] before:w-[140px] before:translate-x-1 before:translate-y-[-10px] before:rotate-[-32deg] before:rounded-full before:bg-gradient-to-r before:from-[#AB1D1C] before:to-[#E18317] before:opacity-30 before:blur-[100px] before:content-[''] lg:before:h-[700px] lg:before:w-[240px] lg:before:translate-x-[-100px]" />
 
-    <Image src="https://tiesen.id.vn/images/tiesen.png" width={2500} height={400} alt="tiesen" />
+    <Image src="https://tiesen.id.vn/assets/tiesen.png" width={2500} height={400} alt="tiesen" />
 
     <Typography level="h1" className="text-center brightness-150">
       A Next.js template with{' '}
